8f58ebfb22571552020159631f38f1cbb8d85756
Patrick510 <patrickdedinho321xz@gmail.com>
2024-10-29 20:36:26 -0400

    Enhance TodoItem layout by adding gap between Checkbox and name

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -5,7 +5,7 @@ import { Checkbox } from "../ui/checkbox";
 export default function TodoItem({ name }: { name: string }) {
   return (
     <div className="flex items-center justify-between p-3 border rounded-lg shadow-sm bg-gray-50">
-      <h3>
+      <h3 className="flex items-center gap-2">
         <Checkbox /> {name}
       </h3>
       <Button variant={"outline"}>
