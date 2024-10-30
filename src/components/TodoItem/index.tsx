9d5d04cd91255e5e8e02e9f3a27346c5697e4dea
Patrick510 <patrickdedinho321xz@gmail.com>
2024-10-29 20:35:04 -0400

    Add Checkbox to TodoItem component

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,10 +1,13 @@
 import { Trash2Icon } from "lucide-react";
 import { Button } from "../ui/button";
+import { Checkbox } from "../ui/checkbox";
 
 export default function TodoItem({ name }: { name: string }) {
   return (
     <div className="flex items-center justify-between p-3 border rounded-lg shadow-sm bg-gray-50">
-      <h3>{name}</h3>
+      <h3>
+        <Checkbox /> {name}
+      </h3>
       <Button variant={"outline"}>
         <Trash2Icon />
       </Button>
