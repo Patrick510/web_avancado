8ee6c7e6f7218a06449c177b1e4e02d50bae6737
Patrick510 <patrickdedinho321xz@gmail.com>
2024-10-29 20:52:50 -0400

    Add checkbox functionality to TodoItem component with state management

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,14 +1,35 @@
 import { Trash2Icon } from "lucide-react";
 import { Button } from "../ui/button";
 import { Checkbox } from "../ui/checkbox";
+import { useState } from "react";
 
-export default function TodoItem({ name }: { name: string }) {
+export default function TodoItem({
+  name,
+  remover,
+}: {
+  name: string;
+  remover: () => void;
+}) {
+  const [checked, setChecked] = useState(false);
+
+  const handleCheck = () => {
+    setChecked(!checked);
+  };
+
   return (
-    <div className="flex items-center justify-between p-3 border rounded-lg shadow-sm bg-gray-50">
+    <div
+      className={`flex items-center justify-between p-3 border rounded-lg shadow-sm ${
+        checked ? "bg-gray-50" : "bg-gray-100"
+      }`}
+    >
       <h3 className="flex items-center gap-2">
-        <Checkbox className="border border-gray-300 h-5 w-5" /> {name}
+        <Checkbox
+          className="border border-gray-300 h-5 w-5"
+          onClick={handleCheck}
+        />{" "}
+        {name}
       </h3>
-      <Button variant={"outline"}>
+      <Button variant={"outline"} onClick={remover}>
         <Trash2Icon />
       </Button>
     </div>
