2f56124a919fb0edf86ff5a2707d4034bb5e09c3
Patrick510 <patrickdedinho321xz@gmail.com>
2024-10-29 20:25:46 -0400

    Implement form submission for task input in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,18 +3,30 @@ import TodoItem from "@/components/TodoItem";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [itens, setItens] = useState([] as string[]);
+
+  function submeterFormulario(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("Submetendo formulário:", e);
+    const f = e.target as HTMLElement;
+    const input = f.querySelector("input") as HTMLInputElement;
+  }
+
   return (
     <div className="p-5 grid gap-3">
       <div>
         <h2>Menu</h2>
       </div>
       <Separator />
-      <div className="flex w-full justify-center items-center gap-2">
-        <Input type="email" placeholder="Email" />
-        <Button type="submit">Adicionar</Button>
-      </div>
+      <form onSubmit={(e) => submeterFormulario(e)}>
+        <div className="flex w-full justify-center items-center gap-2">
+          <Input type="text" placeholder="Escreva uma tarefa" />
+          <Button type="submit">Adicionar</Button>
+        </div>
+      </form>
       <TodoItem name="Comprar pão" />
       <TodoItem name="Fazer trabalho de Algoritmos" />
       <TodoItem name="Paradalal alguma" />
